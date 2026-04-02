229a0f4f125d08a14784e9f37dcc05eef848a4a3
Chancie-Van <vanchancie@gmail.com>
2026-04-02 10:55:47 -0600

    new class and test

--- src/SampleClassTest.java
+++ src/SampleClassTest.java
@@ -1,65 +1,20 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
- * The test class for SampleClass.
- * @author Dr. Jody Paul
- * @version Demonstration
+ * Unit tests for SampleClass.
  */
-public class SampleClassTest extends junit.framework.TestCase {
-    /** The answer to the ultimate question. */
-    private static final int ANSWER = 42;
-    
-    /** Constructor. */
-    public SampleClassTest() { }
+public class SampleClassTest {
 
-    /**
-     * Set up the test fixture.
-     * Called before every test case method.
-     */
-    @BeforeEach
-    protected void setUp() { }
-
-    /**
-     * Tear down the test fixture.
-     * Called after every test case method.
-     */
-    @AfterEach
-    protected void tearDown() { }
-
-    /**
-     * Test the toggle() method.
-     */
     @Test
-    public void testToggle() {
+    public void testAdditionEqualsFive() {
         SampleClass sc = new SampleClass();
-        assertTrue(sc.toggle());
-        assertFalse(sc.toggle());
-        assertTrue(sc.toggle());
+        assertEquals(5, sc.add(2, 3));
     }
 
-    /**
-     * Simple tests of fortyTwo method.
-     * Includes one invalid test.
-     */
     @Test
-    public void testFortyTwo() {
-        SampleClass sm = new SampleClass();
-        assertEquals(ANSWER, sm.fortyTwo(0));
-        assertEquals(0, sm.fortyTwo(-ANSWER));
-        assertEquals(ANSWER - 1, sm.fortyTwo(0));
-        assertEquals(1, sm.fortyTwo(-41));
-    }
-
-    /**
-     * Tests of the strangeMethod.
-     */
-    @Test
-    public void testStrangeMethod() {
-        SampleClass sm = new SampleClass();
-        assertTrue(0 < sm.strangeMethod(1));
-        assertTrue(0 < sm.strangeMethod(-100));
+    public void testAdditionEqualsTen() {
+        SampleClass sc = new SampleClass();
+        assertEquals(10, sc.add(7, 3));
     }
 }
